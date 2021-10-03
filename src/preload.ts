e1d43dbed561c93389938ef281dd35179942f452
Philip Wright <wrightphilip4@gmail.com>
2021-10-03 13:10:22 +0100

    Pull in hostname cmd output

--- src/preload.ts
+++ src/preload.ts
@@ -1,6 +1,9 @@
 import { contextBridge } from 'electron';
 import { cpus } from 'os';
+import { execSync } from 'child_process';
 
+const hostNameOutput = execSync('hostnamectl', { encoding: 'utf-8' });
 contextBridge.exposeInMainWorld('api', {
+  hostNameOutput,
   threads: cpus().length,
 });
