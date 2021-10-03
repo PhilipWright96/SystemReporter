7610b67951a576190fe684b2eda344ea8901b27b
Philip Wright <wrightphilip4@gmail.com>
2021-10-03 13:16:41 +0100

    Use hostname cmd results in HTML

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -1,8 +1,32 @@
-console.log('hello from the renderer process');
+function buildJSONFromCommandLineOutput(commandLineOutput: string) {
+  const xAsBreaks = commandLineOutput.replace(/\n+/g, 'XXX');
+
+  const trimmedWhitespace = xAsBreaks.replace(/\s+/g, ' ').trim();
+
+  const arrayofKeysAndValues = trimmedWhitespace
+    .split(/[:XXX]+/)
+    .filter(Boolean);
+
+  const hostNameMap: Record<string, any> = {};
+
+  for (let i = 0; i < arrayofKeysAndValues.length; i += 2) {
+    const key = arrayofKeysAndValues[i].replaceAll(' ', '');
+    const value = arrayofKeysAndValues[i + 1].trim();
+    hostNameMap[key] = value;
+  }
+
+  return hostNameMap;
+}
 
 // api exposed in preload.ts, but redeclared here to make eslint happy
 declare const api: any;
 
 const coreCount = document.getElementById('cores');
+const hostnameTag = document.getElementById('hostname');
 
-coreCount!.innerText = `Core Count ${api.threads}`;
+const { hostNameOutput } = api;
+
+const hostNameMap = buildJSONFromCommandLineOutput(hostNameOutput);
+
+coreCount!.innerText = `Core Count: ${api.threads}`;
+hostnameTag!.innerText = `Hostname:  ${hostNameMap.Statichostname}`;
