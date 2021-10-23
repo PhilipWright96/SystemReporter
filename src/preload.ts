c0e37371257f575481bb3c8c409aa8aadc5aa128
Philip Wright <wrightphilip4@gmail.com>
2021-10-23 17:22:16 +0100

    Use actual function inside preload

--- src/preload.ts
+++ src/preload.ts
@@ -2,8 +2,12 @@ import { contextBridge } from 'electron';
 import { cpus } from 'os';
 import { execSync } from 'child_process';
 
+function main(): void {
 const hostNameOutput = execSync('hostnamectl', { encoding: 'utf-8' });
 contextBridge.exposeInMainWorld('api', {
   hostNameOutput,
   threads: cpus().length,
 });
+}
+
+main();
