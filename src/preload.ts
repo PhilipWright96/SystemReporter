b9c5da34dd00c875206a03155c929a0f03d78786
Philip Wright <wrightphilip4@gmail.com>
2021-10-23 17:26:01 +0100

    Move json parsing to preload

--- src/preload.ts
+++ src/preload.ts
@@ -2,10 +2,36 @@ import { contextBridge } from 'electron';
 import { cpus } from 'os';
 import { execSync } from 'child_process';
 
+function buildJSONFromCommandLineOutput(
+  commandLineOutput: string
+): Record<string, any> {
+  const hostNameMap: Record<string, any> = {};
+
+  // Add XXX as a placeholder to easily distinguish different outputs of the command
+  const cleanedStringOutput = commandLineOutput
+    .replace(/\n+/g, 'XXX')
+    .replace(/\s+/g, ' ')
+    .trim();
+
+  const outputKeysAndValues = cleanedStringOutput
+    .split(/[:XXX]+/)
+    .filter(Boolean);
+
+  for (let i = 0; i < outputKeysAndValues.length; i += 2) {
+    const key = outputKeysAndValues[i].replaceAll(' ', '');
+    const value = outputKeysAndValues[i + 1].trim();
+    hostNameMap[key] = value;
+  }
+
+  return hostNameMap;
+}
+
 function main(): void {
 const hostNameOutput = execSync('hostnamectl', { encoding: 'utf-8' });
+  const hostNameMap = buildJSONFromCommandLineOutput(hostNameOutput);
+
 contextBridge.exposeInMainWorld('api', {
-  hostNameOutput,
+    hostNameMap,
   threads: cpus().length,
 });
 }
--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -1,37 +1,11 @@
 // api exposed in preload.ts, but redeclared here to make eslint happy
 declare const api: any;
 
-function buildJSONFromCommandLineOutput(
-  commandLineOutput: string
-): Record<string, any> {
-  const hostNameMap: Record<string, any> = {};
-
-  // Add XXX as a placeholder to easily distinguish different outputs of the command
-  const cleanedStringOutput = commandLineOutput
-    .replace(/\n+/g, 'XXX')
-    .replace(/\s+/g, ' ')
-    .trim();
-
-  const outputKeysAndValues = cleanedStringOutput
-    .split(/[:XXX]+/)
-    .filter(Boolean);
-
-  for (let i = 0; i < outputKeysAndValues.length; i += 2) {
-    const key = outputKeysAndValues[i].replaceAll(' ', '');
-    const value = outputKeysAndValues[i + 1].trim();
-    hostNameMap[key] = value;
-  }
-
-  return hostNameMap;
-}
-
 function setDyanmicValues(): void {
   const coreCount = document.getElementById('cores');
   const hostnameTag = document.getElementById('hostname');
 
-  const { hostNameOutput } = api;
-
-  const hostNameMap = buildJSONFromCommandLineOutput(hostNameOutput);
+  const { hostNameMap } = api;
 
   coreCount!.innerText = `Core Count: ${api.threads}`;
   hostnameTag!.innerText = `Hostname:  ${hostNameMap.Statichostname}`;
