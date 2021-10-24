f14ca6f39afd8976aeb0746486b7bbf7e5874901
Philip Wright <wrightphilip4@gmail.com>
2021-10-24 16:57:00 +0100

    Improve function names

--- src/preload.ts
+++ src/preload.ts
@@ -3,7 +3,7 @@ import { cpus } from 'os';
 import { execSync } from 'child_process';
 import buildJSONFromCommandLineOutput from './stringUtil';
 
-function main(): void {
+function exposeMachineStatistics(): void {
   const hostNameOutput = execSync('hostnamectl', { encoding: 'utf-8' });
   // TODO: Investigate using 'process'object values here as well
   const hostNameMap = buildJSONFromCommandLineOutput(hostNameOutput);
@@ -14,4 +14,4 @@ function main(): void {
   });
 }
 
-main();
+exposeMachineStatistics();
