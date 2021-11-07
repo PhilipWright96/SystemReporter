d772600d09db0a6bcad11cadeca7533ed74a493a
Philip Wright <wrightphilip4@gmail.com>
2021-11-07 15:39:57 +0000

    Extract out setting of CPU values on HTML

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -1,15 +1,23 @@
 // api exposed in preload.ts, but redeclared here to make eslint happy
 declare const api: any;
 
-function setDyanmicValues(): void {
+function setCPUValues(lscpuMap: Record<string, any>): void {
   const coreCount = document.getElementById('cores');
-  const hostnameTag = document.getElementById('hostname');
   const architecture = document.getElementById('architecture');
-
-  const { hostNameMap, lscpuMap } = api;
+  const vendor = document.getElementById('vendor');
 
   architecture!.innerText = `Architecture: ${lscpuMap.Architecture}`;
   coreCount!.innerText = `Core Count: ${api.threads}`;
+  vendor!.innerText = `Vendor:  ${lscpuMap.VendorID}`;
+}
+
+function setDyanmicValues(): void {
+  const hostnameTag = document.getElementById('hostname');
+
+  const { hostNameMap, lscpuMap } = api;
+
+  setCPUValues(lscpuMap);
+
   hostnameTag!.innerText = `Hostname:  ${hostNameMap.Statichostname}`;
 }
 
