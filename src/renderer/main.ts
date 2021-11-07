e56b4c791750f96f3d9786e229a951aaa371b275
Philip Wright <wrightphilip4@gmail.com>
2021-11-07 16:53:30 +0000

    Show min and max mhz

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -4,11 +4,15 @@ declare const api: any;
 function setCPUValues(lscpuMap: Record<string, any>): void {
   const architecture = document.getElementById('architecture');
   const coreCount = document.getElementById('cores');
+  const minMHz = document.getElementById('min-mhz');
+  const maxMHz = document.getElementById('max-mhz');
   const modelName = document.getElementById('model-name');
   const vendor = document.getElementById('vendor');
 
   architecture!.innerText = `Architecture: ${lscpuMap.Architecture}`;
   coreCount!.innerText = `Core Count: ${lscpuMap.CPUs}`;
+  minMHz!.innerText = `Min MHz: ${lscpuMap.CPUminMHz}`;
+  maxMHz!.innerText = `Max MHz: ${lscpuMap.CPUmaxMHz}`;
   modelName!.innerText = `Model Name: ${lscpuMap.Modelname}`;
   vendor!.innerText = `Vendor:  ${lscpuMap.VendorID}`;
 }
