8e1ef81a3d9e8114d946f69a18d463302e6cdc50
Philip Wright <wrightphilip4@gmail.com>
2021-11-21 16:07:31 +0000

    Refactor buildJSON to allow filtering keys

--- src/stringUtil.ts
+++ src/stringUtil.ts
@@ -1,5 +1,6 @@
 function buildJSONFromCommandLineOutput(
-  commandLineOutput: string
+  commandLineOutput: string,
+  supportedValues: string[] = []
 ): Record<string, any> {
   const hostNameMap: Record<string, any> = {};
 
@@ -14,11 +15,15 @@ function buildJSONFromCommandLineOutput(
     .filter(Boolean);
 
   for (let i = 0; i < outputKeysAndValues.length; i += 2) {
+    // Remove space and brackets from string for valid object key
     const key = outputKeysAndValues[i]
       .replaceAll(' ', '')
       .replaceAll(/[\(\)']+/g, '');
     const value = outputKeysAndValues[i + 1].trim();
-    hostNameMap[key] = value;
+
+    if (supportedValues.length === 0 || supportedValues.includes(key)) {
+      hostNameMap[key] = value;
+    }
   }
 
   return hostNameMap;
