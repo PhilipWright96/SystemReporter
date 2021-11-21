04be248c34374a70deba591dda50c737949cbdaf
Philip Wright <wrightphilip4@gmail.com>
2021-11-21 16:22:27 +0000

    Rename variables

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -2,11 +2,11 @@
 declare const api: any;
 
 function setObjectValuesOnHtml(valuesToSetOnHtml: Record<string, any>): void {
-  Object.entries(valuesToSetOnHtml).forEach((commandOutput) => {
-    const commandOutputKey = commandOutput[0],
-      commandOutputResult = commandOutput[1],
-      htmlTag = document.getElementById(commandOutputKey);
-    htmlTag!.innerText = `${htmlTag!.innerText}: ${commandOutputResult}`;
+  Object.entries(valuesToSetOnHtml).forEach((entry) => {
+    const key = entry[0],
+      value = entry[1],
+      htmlTag = document.getElementById(key);
+    htmlTag!.innerText = `${htmlTag!.innerText}: ${value}`;
   });
 }
 
