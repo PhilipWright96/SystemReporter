11d1628f3eaf4f82dc0e0e132c03e76cbd738c5f
Philip Wright <wrightphilip4@gmail.com>
2021-11-21 15:42:07 +0000

    Use new method for identifier values as well

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -10,37 +10,32 @@ function setObjectValuesOnHtml(
     const commandOutputResult = commandOutput[1];
     if (supportedKeys.includes(commandOutputKey)) {
       const htmlTag = document.getElementById(commandOutputKey);
-      htmlTag!.innerText = `${htmlTag!.innerText}:${commandOutputResult}`;
+      htmlTag!.innerText = `${htmlTag!.innerText}: ${commandOutputResult}`;
     }
   });
 }
 
 function setCPUValues(lscpuMap: Record<string, any>): void {
-  const supportedCpuValueOutputKeys = [
+  setObjectValuesOnHtml(lscpuMap, [
     'Architecture',
     'CPUs',
     'CPUminMHz',
     'CPUmaxMHz',
     'Modelname',
     'VendorID',
-  ];
-
-  setObjectValuesOnHtml(lscpuMap, supportedCpuValueOutputKeys);
+  ]);
 }
 
 function setIdentifierValues(
   hostNameMap: Record<string, any>,
   ipAddressMap: Record<string, any>
 ): void {
-  const hostnameTag = document.getElementById('hostname');
-  const publicIpv4AddressTag = document.getElementById('public-ipv4-address');
-  const privateIpv4AddressTag = document.getElementById('private-ipv4-address');
-  const privateIpv6AddressTag = document.getElementById('private-ipv6-address');
-
-  hostnameTag!.innerText = `Hostname:  ${hostNameMap.Statichostname}`;
-  publicIpv4AddressTag!.innerText = `Public IPv4 Address: ${ipAddressMap.publicIpv4Address}`;
-  privateIpv4AddressTag!.innerText = `Private IPv4 Address: ${ipAddressMap.privateIpv4Address}`;
-  privateIpv6AddressTag!.innerText = `IPv6 Address: ${ipAddressMap.privateIpv6Address}`;
+  setObjectValuesOnHtml(hostNameMap, ['Statichostname']);
+  setObjectValuesOnHtml(ipAddressMap, [
+    'publicIpv4Address',
+    'privateIpv4Address',
+    'privateIpv6Address',
+  ]);
 }
 
 function setDyanmicValues(): void {
