5d26b8505c72a78bbfaf93d96831a7869f741df1
Philip Wright <wrightphilip4@gmail.com>
2022-01-16 15:21:12 +0000

    Break down large method into privates

--- src/preload.ts
+++ src/preload.ts
@@ -7,14 +7,53 @@ import {
 } from './util';
 
 function exposeMachineStatistics(): void {
-  const hostNameOutput = execSync('hostnamectl', { encoding: 'utf-8' }),
-    lscpuOutput = execSync('lscpu', { encoding: 'utf-8' }),
-    privateIpv4Address = execSync('hostname -I', { encoding: 'utf-8' }),
+  // TODO: Investigate using 'process'object values here as well
+  const hostNameMap = getHostNameInformation();
+  const ipAddressMap = getIPAddressInformation();
+  const lscpuMap = getCPUInformation();
+  const memoryMap = getMemoryInformation();
+
+  contextBridge.exposeInMainWorld('api', {
+    hostNameMap,
+    ipAddressMap,
+    lscpuMap,
+    memoryMap,
+  });
+}
+
+function getHostNameInformation() {
+  const hostNameOutput = execSync('hostnamectl', { encoding: 'utf-8' });
+  return buildJSONFromCommandLineOutput(hostNameOutput, ['Statichostname']);
+}
+
+function getIPAddressInformation() {
+  const privateIpv4Address = execSync('hostname -I', { encoding: 'utf-8' }),
     publicIpv4Address = execSync('curl ifconfig.me', { encoding: 'utf-8' }),
     ipv6Command =
       "ip addr | grep inet6 | grep -vwE '(host)' | awk '{print $2;}' | tr -d '\r\n'",
-    privateIpv6Address = execSync(ipv6Command, { encoding: 'utf-8' }),
-    ramMemoryInfo = execSync('cat /proc/meminfo', { encoding: 'utf-8' }),
+    privateIpv6Address = execSync(ipv6Command, { encoding: 'utf-8' });
+
+  return Object.freeze({
+    privateIpv4Address,
+    publicIpv4Address,
+    privateIpv6Address,
+  });
+}
+
+function getCPUInformation() {
+  const lscpuOutput = execSync('lscpu', { encoding: 'utf-8' });
+  return buildJSONFromCommandLineOutput(lscpuOutput, [
+    'Architecture',
+    'CPUs',
+    'CPUminMHz',
+    'CPUmaxMHz',
+    'Modelname',
+    'VendorID',
+  ]);
+}
+
+function getMemoryInformation() {
+  const ramMemoryInfo = execSync('cat /proc/meminfo', { encoding: 'utf-8' }),
     isHardDrive = execSync('cat /sys/block/sda/queue/rotational', {
       encoding: 'utf-8',
     }),
@@ -22,29 +61,10 @@ function exposeMachineStatistics(): void {
       encoding: 'utf-8',
     });
 
-  // TODO: Investigate using 'process'object values here as well
-  const hostNameMap = buildJSONFromCommandLineOutput(hostNameOutput, [
-      'Statichostname',
-    ]),
-    lscpuMap = buildJSONFromCommandLineOutput(lscpuOutput, [
-      'Architecture',
-      'CPUs',
-      'CPUminMHz',
-      'CPUmaxMHz',
-      'Modelname',
-      'VendorID',
-    ]),
-    ipAddressMap = Object.freeze({
-      privateIpv4Address,
-      publicIpv4Address,
-      privateIpv6Address,
-    }),
-    memoryMap = setMapValuesToNewMeasurement(
-      buildJSONFromCommandLineOutput(ramMemoryInfo, [
-        'MemTotal',
-        'MemAvailable',
-      ])
-    );
+  const memoryMap = setMapValuesToNewMeasurement(
+    buildJSONFromCommandLineOutput(ramMemoryInfo, ['MemTotal', 'MemAvailable'])
+  );
+
   memoryMap.PersistantStorageType =
     parseInt(isHardDrive) == 0 ? 'Solid State Drive' : 'Hard Drive';
 
@@ -61,12 +81,7 @@ function exposeMachineStatistics(): void {
 
   memoryMap.TotalDiskCapacity = `${totalSizeOfDriveSpace} MB`;
 
-  contextBridge.exposeInMainWorld('api', {
-    hostNameMap,
-    ipAddressMap,
-    lscpuMap,
-    memoryMap,
-  });
+  return memoryMap;
 }
 
 exposeMachineStatistics();
