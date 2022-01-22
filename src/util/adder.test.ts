39ed84f1be764e9758f66b8b91783f70b02178a3
Philip Wright <wrightphilip4@gmail.com>
2022-01-22 15:01:35 +0000

    Add dummy example test and file

---
+++ src/util/adder.test.ts
@@ -0,0 +1,10 @@
+import { addTwoNumbers } from './adder';
+
+describe('test add function', () => {
+  it('should return correct result for adding two numbers', () => {
+    // Given
+    // When
+    // Then
+    expect(addTwoNumbers(10, 5)).toBe(15);
+  });
+});
---
+++ src/util/adder.ts
@@ -0,0 +1,4 @@
+function addTwoNumbers(a: number, b: number) {
+  return a + b;
+}
+export { addTwoNumbers };
