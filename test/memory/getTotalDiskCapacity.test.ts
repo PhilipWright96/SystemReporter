ca3eaf9c37a86d08d7c2a85194ff03ef3645364e
Philip Wright <wrightphilip4@gmail.com>
2022-05-03 21:43:54 +0100

    Add test for get total disk capacity

---
+++ test/memory/getTotalDiskCapacity.test.ts
@@ -0,0 +1,51 @@
+import childProcess from 'child_process';
+import { getTotalDiskCapacity } from '../../src/memory/getTotalDiskCapacity';
+import * as stringParseUtil from '../../src/util/stringParseUtil';
+
+jest.mock('child_process');
+
+describe('getTotalDiskCapacity', () => {
+  it('should return total size of dev/sda file systems', () => {
+    // Given
+    const fakeCommandLineOutput = 'test',
+      fakeFileSystemToSize = Object.freeze({
+        'dev/sda': 5,
+        'dev/notSda': 10,
+      });
+
+    jest
+      .spyOn(childProcess, 'execSync')
+      .mockReturnValueOnce(Buffer.from(fakeCommandLineOutput, 'utf-8'));
+
+    jest
+      .spyOn(stringParseUtil, 'getFileSystemSizeInfo')
+      .mockReturnValueOnce(fakeFileSystemToSize);
+
+    // When
+    const returnedDiskCapacity = getTotalDiskCapacity();
+
+    // Then
+    expect(childProcess.execSync).toHaveBeenCalledTimes(1);
+    expect(childProcess.execSync).toHaveBeenCalledWith(
+      'df -H --output=source,size',
+      {
+        encoding: 'utf-8',
+      }
+    );
+
+    expect(childProcess.execSync).toHaveBeenCalledTimes(1);
+    expect(childProcess.execSync).toHaveBeenCalledWith(
+      'df -H --output=source,size',
+      {
+        encoding: 'utf-8',
+      }
+    );
+
+    expect(stringParseUtil.getFileSystemSizeInfo).toHaveBeenCalledTimes(1);
+    expect(stringParseUtil.getFileSystemSizeInfo).toHaveBeenCalledWith(
+      Buffer.from(fakeCommandLineOutput, 'utf-8')
+    );
+
+    expect(returnedDiskCapacity).toStrictEqual('5 MB');
+  });
+});
