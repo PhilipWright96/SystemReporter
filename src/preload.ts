a8dc55dff38945e7804567954caac9cf501e4bab
Philip Wright <wrightphilip4@gmail.com>
2023-05-30 20:10:32 +0100

    Add process info to preload file

--- src/preload.ts
+++ src/preload.ts
@@ -4,19 +4,22 @@ import { getCPUInformation } from './cpu/cpuInfo';
 import { getHostNameInformation } from './hostname/hostnameInfo';
 import { getIPAddressInformation } from './ipaddress/ipaddressInfo';
 import { getMemoryInformation } from './memory/memoryInfo';
+import { getProcessInformation } from './processes/processInfo';
 
 function exposeMachineStatistics(): void {
   // TODO: Investigate using 'process'object values here as well
   const cpuInformation = getCPUInformation(),
     hostNameInformation = getHostNameInformation(),
     idAddressInformation = getIPAddressInformation(),
-    memoryInformation = getMemoryInformation();
+    memoryInformation = getMemoryInformation(),
+    processInformation = getProcessInformation();
 
   contextBridge.exposeInMainWorld('api', {
     cpuInformation,
     hostNameInformation,
     idAddressInformation,
     memoryInformation,
+    processInformation,
   });
 }
 
